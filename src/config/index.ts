3a15d2f8d14bfe658800dd27b0a466b2b7b7a111
sidpd51 <sidpd510@gmail.com>
2025-04-27 01:32:36 +0530

    feat: add MySQL and Sequelize integration
    - Added mysql2 and sequelize dependencies to package.json.
    - Created a new config file for database configuration using environment variables.
    - Updated index.ts to include database configuration.
    - Added a migration file to create the hotels table in the database.
    - Implemented model index file to initialize Sequelize models.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,15 @@
 // This file contains all the basic configuration logic for the app server to work
 import dotenv from 'dotenv';
 
-type ServerConfig = {
+type ServerConfigType = {
     PORT: number
+}
+
+type DbConfigType = {
+    DB_HOST: string,
+    DB_USER: string,
+    DB_PASSWORD: string,
+    DB_NAME: string
 }
 
 function loadEnv() {
@@ -12,6 +19,13 @@ function loadEnv() {
 
 loadEnv();
 
-export const serverConfig: ServerConfig = {
+export const serverConfig: ServerConfigType = {
     PORT: Number(process.env.PORT) || 3001
+};
+
+export const dbConfig: DbConfigType = {
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_USER: process.env.DB_USER || 'root',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'root@123',
+    DB_NAME: process.env.DB_NAME || 'airbnb_development'
 };
