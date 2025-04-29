d832bbf370d5a31ccaff6a23a34754e68e17768d
sidpd51 <sidpd510@gmail.com>
2025-04-29 21:31:58 +0530

    feat: add http-status-codes dependency and update response status in hotel handlers

--- src/controllers/hotel.controller.ts
+++ src/controllers/hotel.controller.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { createHotelService, getHotelByIdService } from "../services/hotel.service";
 import { NotFoundError } from "../utils/errors/app.error";
+import { StatusCodes } from "http-status-codes";
 
 export const createHotelHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const hotelData = req.body;
         const hotelResponse = await createHotelService(hotelData);
-        res.status(201).json({
+        res.status(StatusCodes.CREATED).json({
             message: "Hotel created successfully",
             data: hotelResponse,
             success: true,
@@ -23,7 +24,7 @@ export const getHotelByIdHandler = async (req: Request, res: Response, next: Nex
         if (!hotelResponse) {
             throw new NotFoundError("Hotel not found");
         }
-        res.status(200).json({
+        res.status(StatusCodes.OK).json({
             message: "Hotel retrieved successfully",
             data: hotelResponse,
             success: true,
@@ -31,4 +32,17 @@ export const getHotelByIdHandler = async (req: Request, res: Response, next: Nex
     } catch (error) {
         next(error);
     }
+}
+
+export const getAllHotelsHandler = async (req: Request, res: Response, next: NextFunction) => {
+    res.status(StatusCodes.NOT_IMPLEMENTED)
+}
+
+
+export const updateHotelHandler = async (req: Request, res: Response, next: NextFunction) => {
+    res.status(StatusCodes.NOT_IMPLEMENTED)
+}
+
+export const deleteHotelHandler = async (req: Request, res: Response, next: NextFunction) => {
+    res.status(StatusCodes.NOT_IMPLEMENTED)
 }
