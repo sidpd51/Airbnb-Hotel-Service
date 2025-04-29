7fa94dc86b0b45c30f6c4f46de5a86a550c46f86
sidpd51 <sidpd510@gmail.com>
2025-04-29 20:08:03 +0530

    feat: add NotFoundError handling in getHotelByIdHandler

--- src/controllers/hotel.controller.ts
+++ src/controllers/hotel.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { createHotelService, getHotelByIdService } from "../services/hotel.service";
+import { NotFoundError } from "../utils/errors/app.error";
 
 export const createHotelHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -19,6 +20,9 @@ export const getHotelByIdHandler = async (req: Request, res: Response, next: Nex
     try {
         const hotelId = parseInt(req.params.id);
         const hotelResponse = await getHotelByIdService(hotelId);
+        if (!hotelResponse) {
+            throw new NotFoundError("Hotel not found");
+        }
         res.status(200).json({
             message: "Hotel retrieved successfully",
             data: hotelResponse,
